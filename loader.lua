bcbc130720bdc04ca8a6bb56f9b359afdc33257e
Nev <153566382+outrozl@users.noreply.github.com>
2023-12-31 19:40:27 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,13 @@
---despues
+-- Credits vynixus for the loader
+
+-- Creditos a vynixus por el cargador
+local games = {
+    [{2753915549, 4442272183, 7449423635}] = ""
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
