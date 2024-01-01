e78532a7ce4ce2de93cbee47221d7b2685329dd1
Nev <153566382+outrozl@users.noreply.github.com>
2024-01-01 03:06:01 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,8 @@
 -- Creditos a vynixus por el cargador
 local games = {
     [{2753915549, 4442272183, 7449423635}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/bf.lua",
-    [{142823291}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/mm2.lua"
+    [{142823291}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/mm2.lua",
+    [{3101667897}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/los.lua"
 }
 
 for ids, url in next, games do
