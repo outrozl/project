8cf7e063ad6c9cabb866eb2d750534431f0442ac
Nev <153566382+outrozl@users.noreply.github.com>
2024-01-01 03:51:47 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,15 +2,18 @@
 
 -- Creditos a vynixus por el cargador
 local games = {
-    [{2753915549, 4442272183, 7449423635}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/bf.lua",
-    [{142823291}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/mm2.lua",
-    [{3101667897}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/los.lua",
-    [{13772394625}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/BB.lua"
+  [{2753915549, 4442272183, 7449423635}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/bf.lua",
+  [{142823291}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/mm2.lua",
+  [{3101667897}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/los.lua",
+  [{13772394625}] = "https://raw.githubusercontent.com/outrozl/project/main/scripts/BB.lua"
 }
 
 for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        break
-    end
+  if table.find(ids, game.PlaceId) then
+    loadstring(game:HttpGet(url))()
+    break
+  else
+    game:ShowError("Error: No se ha podido encontrar el juego")
+  end
 end
+
