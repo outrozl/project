773ad1643c826e40d8d2837da5e5cd8c9cf6aa8f
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-10 15:37:51 -0600

    Update es-ui.lua

--- script-es/es-ui.lua
+++ script-es/es-ui.lua
@@ -1,1 +1,10 @@
+local NevLib = {}
 
+-- Función de ejemplo en la librería
+function NevLib.sayHello()
+    print("¡Hola desde la librería!")
+end
+
+-- Agrega más funciones aquí según tus necesidades
+
+return NevLib
