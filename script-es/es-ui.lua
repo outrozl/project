6d43287dd02658aa8fee86956d81233facf6f4f2
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-10 15:40:16 -0600

    Update es-ui.lua

--- script-es/es-ui.lua
+++ script-es/es-ui.lua
@@ -1,5 +1,5 @@
 local NevLib = {}
-
+print("NevLib: Este script usa la libreria de outrozl / just_nev_dev")
 -- Función de ejemplo en la librería
 function NevLib.sayHello()
     print("¡Hola desde la librería!")
