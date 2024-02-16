a722fb749aa80f4e4f7128495b648b57a0baf063
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-16 11:46:03 -0600

    Update es-ui.lua

--- script-es/es-ui.lua
+++ script-es/es-ui.lua
@@ -1,13 +1,3 @@
 local Nevlib = {}
 
-function Nevlib.MakeNotify(Titulo, Texto, Icono, Tiempo)
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = Titulo, -- Required
-        Text = Texto, -- Required
-        Icon = Icono, -- Optional
-        Duration = Tiempo
-    })
-end
-
-
 return Nevlib
