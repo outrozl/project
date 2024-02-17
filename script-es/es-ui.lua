72620cc78d72aa2769adfa476c5755ffdcfdfc6a
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-17 17:51:07 -0600

    Update es-ui.lua

--- script-es/es-ui.lua
+++ script-es/es-ui.lua
@@ -1,3 +1,74 @@
-local Nevlib = {}
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 
-return Nevlib
+local GUI = Mercury:Create{
+    Name = "Nev | Script Hub",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/outrozl"
+}
+
+-- Functions
+
+local scripts = {
+    ["Infinite Yield"] = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",
+    ["CMD - X"] = "https://raw.githubusercontent.com/CMD-X/CMD/master/Source",
+}
+
+-- Tabs
+
+local Universals = GUI:Tab{
+	Name = "Universals",
+	Icon = "rbxassetid://6034227139"
+}
+
+-- Content
+
+Universals:Slider{
+    Name = "Velocidad",
+    Default = 16,
+    Min = 0,
+    Max = 500,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end
+}
+
+Universals:Slider{
+    Name = "Salto",
+    Default = 50,
+    Min = 0,
+    Max = 500,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+    end
+}
+
+-- Select game
+
+Universals:Textbox{
+    Name = "Pon el nombre de algun script",
+    Callback = function(text)
+        local scriptName = text
+        local selectedScript = scripts[scriptName]
+        if selectedScript then
+            local success, result = pcall(function()
+                return loadstring(game:HttpGet(selectedScript))()
+            end)
+            if not success then
+                warn("Hubo un error al cargar el script:", result)
+                game:GetService("StarterGui"):SetCore("SendNotification",{
+                    Title = "Nev | Script Hub", -- Required
+                    Text = "Hubo un erro al cargar el script: " .. result .. ".", -- Required
+                    Icon = "rbxassetid://7734053281" -- Optional
+                })
+            end
+        else
+            warn("El nombre del script ingresado no coincide con ningún script disponible.")
+            game:GetService("StarterGui"):SetCore("SendNotification",{
+                Title = "Nev | Script Hub", -- Required
+                Text = "El nombre del script ingresado no coincide con qué script disponible.", -- Required
+                Icon = "rbxassetid://7734053281" -- Optional
+            })
+        end
+    end
+}
