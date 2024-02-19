23f845020e2e503a37b1b8c79607ba764d02575b
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-18 18:54:43 -0600

    Update bf-en.lua

--- nevscripthub/scripts/bf-en.lua
+++ nevscripthub/scripts/bf-en.lua
@@ -1,5 +1,5 @@
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
-local NevVer = "Beta 1.0.0"
+local NevVer = "1.0.0"
 
 local GUI = Mercury:Create{
     Name = "Nev | BF | "..NevVer,
@@ -9,84 +9,53 @@ local GUI = Mercury:Create{
 }
 
 local best = {
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
+    ["Hoho Hub V3"] = { URL = "https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI", Method = 1 },
+    ["Hoho Hub V2"] = { URL = "https://raw.githubusercontent.com/acsu123/HohoV2/main/OldScriptLoad", Method = 1 },
+    ["Uranium X Hub"] = { URL = "https://raw.githubusercontent.com/Augustzyzx/UraniumMobile/main/UraniumKak.lua", Method = 1 },
+    ["CFrame Hub"] = { URL = "https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruitV2.lua", Method = 1 },
+    ["Neva Hub"] = { URL = "https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2'", Method = 1 },
+    ["Makima Hub"] = { URL = "https://raw.githubusercontent.com/Piwwy0909/MakimaRemake/main/Cumback.Makima", Method = 1 },
+    ["Sonic Hub"] = { URL = "https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/Protected-25.lua", Method = 1 },
+    ["Zee Hub"] = { URL = "https://zKuzy.link/ZeeHub.lua", Method = 1 },
+    ["Madox Hub"] = { URL = "https://raw.githubusercontent.com/Fiend1sh/FiendMain/main/MadoxHubKey", Method = 1 },
+    ["2x Hub"] = { URL = "https://raw.githubusercontent.com/armch063/BF-1XLIIHUB-V3/main/README.md", Method = 1 },
+    ["Unique Hub"] = { URL = "https://raw.githubusercontent.com/AkiraNus/UniquehubKak/main/FreeCr.Xenonhub", Method = 1 },
+    ["Chiba Hub"] = { URL = "https://raw.githubusercontent.com/KindIhave/ChibaHubcomeback/main/Chibacomebackbeta.txt", Method = 1 },
+    ["Zaque Hub"] = { URL = "https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub", Method = 1 },
+    ["Glord Hub"] = { URL = "https://raw.githubusercontent.com/GGhostVN/GhostLord/main/BF", Method = 1 },
+    ["Blck Hub"] = { URL = "https://raw.githubusercontent.com/1f0yt/community/main/BLCK", Method = 1 },
+    ["Loli Hub"] = { URL = "https://raw.githubusercontent.com/1f0yt/community/main/neon", Method = 1 },
+    ["Alucard Hub"] = { URL = "https://raw.githubusercontent.com/JJTChannel/Alucard_hub/main/main.lua", Method = 1 },
+    ["Mukuru Hub"] = { URL = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader", Method = 1 },
+    ["Zen Hub"] = { URL = "https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader", Method = 1 },
+    ["Ripper Hub"] = { URL = "https://raw.githubusercontent.com/xDestinyx/RipperHub/main/Loader.lua", Method = 1 },
+    ["Ren Hub"] = { URL = "https://raw.githubusercontent.com/UltraStuff/scripts2/main/ren", Method = 1 },
+    ["Void Hub"] = { URL = "https://raw.githubusercontent.com/Efe0626/VoidHub/main/Script", Method = 1 },
+    ["BlackTrap Gui"] = { URL = "https://gitlab.com/Sky2836/test/-/raw/main/Script_All_In-One", Method = 1 },
+    ["More Scripts Soon!!"] = { URL = "", Method = 1 },
 }
 
 local mobile = {
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
+    ["Destiny Hub"] = { URL = "https://raw.githubusercontent.com/KudoCon/DestinyHub/main/DestinyKey", Method = 1 },
+    ["Atomic Hub"] = { URL = "https://raw.githubusercontent.com/ArceusXHub/atomic-hub/main/atomic-hub.lua", Method = 1 },
+    ["Zamex Hub"] = { URL = "https://raw.githubusercontent.com/Sixnumz/ZamexMobile/main/Zamex_Mobile.lua", Method = 1 },
+    ["Saki Hub"] = { URL = "https://sharetext.me/raw/u3if6m3hgr", Method = 1 },
+    ["Speed Hub X"] = { URL = "https://raw.githubusercontent.com/ahmadsgamer2/Script--Game/main/Script%20Game", Method = 1 },
+    ["Uranium X Hub"] = { URL = "https://raw.githubusercontent.com/Augustzyzx/UraniumMobile/main/UraniumKak.lua", Method = 1 },
+    ["CFrame Hub"] = { URL = "https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua", Method = 1 },
+    ["More Scripts Soon!!"] = { URL = "", Method = 1 },
 }
 
 local utilitys = {
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
+    ["Fruit Finder and hopper"] = { URL = "https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/BetaFruitFinder", Method = 1 },
+    ["Chest Farm and hopper"] = { URL = "https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/ChestFarmOp", Method = 1 },
 }
 
 local other = {
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
-    [""] = { URL = "", Method = 1 },
+    ["Shadow Hub"] = { URL = "https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF", Method = 1 },
+    ["Playback X Hub"] = { URL = "https://raw.githubusercontent.com/NeaPchX2/Playback-X-HUB/main/Protected.lua.txt", Method = 1 },
+    ["Flare Hub"] = { URL = "https://raw.githubusercontent.com/1f0yt/community/master/flares", Method = 1 },
+    ["More Scripts Soon!!"] = { URL = "", Method = 1 },
 }
 
 -- Functions
@@ -264,7 +233,7 @@ GUI:Credit{
 
 GUI:Notification{
 	Title = "Nev | Script Hub",
-	Text = "Script Loaded Successfully",
+	Text = "Script Loaded Successfully, version: " ..NevVer,
 	Duration = 3,
 	Callback = function() end
 }
