e5855d86916b8f14926165013621d474bfdaa269
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-18 20:18:44 -0600

    Update selectlang.bf.lua

--- nevscripthub/scripts/selectlang.bf.lua
+++ nevscripthub/scripts/selectlang.bf.lua
@@ -1,60 +1,32 @@
-local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local GUI = Mercury:Create{
-    Name = "Nev | BF | Select Lang",
-    Size = UDim2.fromOffset(600, 400),
-    Theme = Mercury.Themes.Dark,
-    Link = "https://github.com/outrozl/project/blob/main/nevscripthub/selectlang"
-}
+local Window = OrionLib:MakeWindow({Name = "Select Language", HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
 
-local es = GUI:Tab{
-	Name = "Selecionar Idioma",
-	Icon = "rbxassetid://6031225812"
-}
+local Tab = Window:MakeTab({
+    Name = "Select Language",
+    Icon = "rbxassetid://6031225812",
+    PremiumOnly = false
+})
 
-local en = GUI:Tab{
-	Name = "Select Language",
-	Icon = "rbxassetid://6031225812"
-}
+Tab:AddDropdown({
+	Name = "Dropdown",
+	Default = nil,
+	Options = {"English", "Español", "Português", "日本語"},
+	Callback = function(Value)
+		if Value == "English" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-en.lua"))()
+            OrionLib:Destroy()
+        elseif Value == "Español" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-es.lua"))()
+            OrionLib:Destroy()
+        elseif Value == "Português" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-pr.lua"))()
+            OrionLib:Destroy()
+        elseif Value == "日本語" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-jp.lua"))()
+            OrionLib:Destroy()
+        end
+	end    
+})
 
-local pr = GUI:Tab{
-	Name = "Seleziona Lingua",
-	Icon = "rbxassetid://6031225812"
-}
-
-local jp = GUI:Tab{
-    Name = "言語を選択",
-    Icon = "rbxassetid://6031225812"
-}
-
-es:Button{
-	Name = "Español",
-	Description = nil,
-	Callback = function() 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-es.lua"))()
-    end
-}
-
-en:Button{
-	Name = "English",
-	Description = nil,
-	Callback = function() 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-en.lua"))()
-    end
-}
-
-pr:Button{
-	Name = "Seleziona",
-	Description = nil,
-	Callback = function() 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-pr.lua"))()
-    end
-}
-
-jp:Button{
-    Name = "言語を選択",
-    Description = nil,
-    Callback = function() 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/bf-jp.lua"))()
-    end
-}
+OrionLib:Init()
