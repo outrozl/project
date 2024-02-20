5f91555c2ba165f218ab582a7d1826be32e9aaa0
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-19 23:36:03 -0600

    Update selectlang.stg.lua

--- nevscripthub/scripts/selectlang.stg.lua
+++ nevscripthub/scripts/selectlang.stg.lua
@@ -1,6 +1,27 @@
+local function GetVer()
+    -- Enlace que contiene solo el número de versión
+    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
+    
+    -- Realizar una solicitud HTTP para obtener el contenido del enlace
+    local response = game.HttpGet(versionURL)
+    
+    -- Verificar si la respuesta es válida
+    if response then
+        -- Extraer el número de versión del texto obtenido
+        local NevVer = response:match("(%d+%.%d+%.%d+)")
+        return NevVer
+    else
+        -- Manejar el caso en que la respuesta no sea válida
+        return nil
+    end
+end
+
+-- Obtener la versión
+local NevVer = GetVer()
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Select Language"..game.Name, HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Select Language"..game.Name.. ""..NevVer, HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
 
 local Tab = Window:MakeTab({
     Name = "Select Language",
@@ -16,13 +37,13 @@ Tab:AddDropdown({
 		if Value == "English" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/stg-en.lua"))()
             OrionLib:Destroy()
-        elseif Value == "Español" then
+        elseif Value == "Español (No esta funcionando)" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/stg-es.lua"))()
             OrionLib:Destroy()
-        elseif Value == "Português" then
+        elseif Value == "Português (não está funcionando)" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/stg-pr.lua"))()
             OrionLib:Destroy()
-        elseif Value == "日本語" then
+        elseif Value == "日本語 (動かない)" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/stg-jp.lua"))()
             OrionLib:Destroy()
         end
