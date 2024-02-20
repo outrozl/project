1f28adb549d47feafac4bf272f699c3733faf22c
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-19 23:42:48 -0600

    Update selectlang.bf.lua

--- nevscripthub/scripts/selectlang.bf.lua
+++ nevscripthub/scripts/selectlang.bf.lua
@@ -1,6 +1,27 @@
+local function GetVer()
+    -- Enlace que contiene solo el número de versión
+    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
+    
+    -- Realizar una solicitud HTTP para obtener el contenido del enlace
+    local response = game.HttpGet(versionURL)
+    
+    -- Verificar si la respuesta es válida
+    if response then
+        -- Extraer el número de versión del texto obtenido
+        local NevVer = response:match("(%d+%.%d+%.%d+)")
+        return NevVer
+    else
+        -- Manejar el caso en que la respuesta no sea válida
+        return nil
+    end
+end
+
+-- Obtener la versión
+local NevVer = GetVer()
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Select Language"..game.Name, HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Select Language "..NevVer, HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
 
 local Tab = Window:MakeTab({
     Name = "Select Language",
