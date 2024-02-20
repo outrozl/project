28ab86d690e9bb54943a0a647d9aee91b0a479fa
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-20 10:01:32 -0600

    Update Newloader.lua

--- Newloader.lua
+++ Newloader.lua
@@ -1,24 +1,33 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local games = {
-  [{2753915549, 4442272183, 7449423635}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.bf.lua?raw=true",
-  [{10449761463}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.stg.lua?raw=true",
-  [{155615604}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.prisonlife.lua?raw=true",
+    [{2753915549, 4442272183, 7449423635}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.bf.lua?raw=true",
+    [{10449761463}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.stg.lua?raw=true",
+    [{155615604}] = "https://github.com/outrozl/project/blob/main/nevscripthub/scripts/selectlang.prisonlife.lua?raw=true",
 }
 
-for ids, url in next, games do
+local gameSupported = false
 
-  if table.find(ids, game.PlaceId) then
-    loadstring(game:HttpGet(url))()
-    break
-  else
-    game:GetService("StarterGui"):SetCore("SendNotification", {
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        gameSupported = true
         OrionLib:MakeNotification({
-	        Name = "Nev | Script Hub",
-	        Content = "Game not supported",
-	        Image = "rbxassetid://7734053281",
-	        Time = 5
+            Name = "Nev | Script Hub",
+            Content = "Game supported, loading script...",
+            Image = "rbxassetid://7734053281",
+            Time = 5
         })
+        wait(5)
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
+
+if not gameSupported then
+    OrionLib:MakeNotification({
+        Name = "Nev | Script Hub",
+        Content = "Game not supported",
+        Image = "rbxassetid://7734053281",
+        Time = 5
     })
-  end
 end
