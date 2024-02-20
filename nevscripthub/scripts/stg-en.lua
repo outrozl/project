ad2853048d1d85539446997193d31a5aa73ec668
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-19 23:34:31 -0600

    Update stg-en.lua

--- nevscripthub/scripts/stg-en.lua
+++ nevscripthub/scripts/stg-en.lua
@@ -1,5 +1,25 @@
+local function GetVer()
+    -- Enlace que contiene solo el número de versión
+    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
+    
+    -- Realizar una solicitud HTTP para obtener el contenido del enlace
+    local response = game.HttpGet(versionURL)
+    
+    -- Verificar si la respuesta es válida
+    if response then
+        -- Extraer el número de versión del texto obtenido
+        local NevVer = response:match("(%d+%.%d+%.%d+)")
+        return NevVer
+    else
+        -- Manejar el caso en que la respuesta no sea válida
+        return nil
+    end
+end
+
+-- Obtener la versión
+local NevVer = GetVer()
+
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
-local NevVer = "1.0.0"
 
 local GUI = Mercury:Create{
     Name = "Nev | STG | "..NevVer,
