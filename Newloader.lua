ef8af275e01a315a8ecf244b6d9d79ccaa8c54d1
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-22 20:24:41 -0600

    Update Newloader.lua

--- Newloader.lua
+++ Newloader.lua
@@ -1,22 +1,3 @@
-local function GetVer()
-    -- Enlace que contiene solo el número de versión
-    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
-    
-    -- Realizar una solicitud HTTP para obtener el contenido del enlace
-    local response = game.HttpGet(versionURL)
-    
-    -- Verificar si la respuesta es válida
-    if response then
-        -- Extraer el número de versión del texto obtenido
-        local NevVer = response:match("(%d+%.%d+%.%d+)")
-        return NevVer
-    else
-        -- Manejar el caso en que la respuesta no sea válida
-        return nil
-    end
-end
-
--- Obtener la versión
-local NevVer = GetVer()
+local NevVer = "1.1.3"
 
 print("Nev | Script Hub | "..NevVer.." : Wait version 1.1.3 remodel of UI and gamechecker")
