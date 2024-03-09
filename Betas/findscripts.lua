b1c8f5de717b9f47b93ae9900baf9b0b34f06b79
Nev <153566382+outrozl@users.noreply.github.com>
2024-03-09 15:36:34 -0600

    Update findscripts.lua

--- Betas/findscripts.lua
+++ Betas/findscripts.lua
@@ -34,37 +34,38 @@ local Tab = Window:MakeTab({
 })
 
 local Tab2 = Window:MakeTab({
-    Name = "Scripts",
+    Name = "Scripts Universales",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
 local Section = Tab:AddSection({
-    Name = "Loader (Use the real name of the script to load)"
+    Name = "Loader (Usa el nombre real para cargar el script)"
 })
 
 Tab:AddTextbox({
-    Name = "Loader: (Only Names)",
+    Name = "Loader: (Solo nombres)",
     Default = "",
     TextDisappear = false,
     Callback = function(Value)
         for name, callback in pairs(scripts) do
             if name == Value then
-                addnotify("Script Loaded", name .. " script executed successfully", "7733710700", 5)
+                addnotify("Script cargado", name .. " script ejecutado con exito (gracias por usar FindScripts)",
+                    "7733710700", 5)
                 callback() -- Execute the script
                 return     -- Exit the loop after a match is found
             end
         end
-        addnotify("Script Not Found", "The script you entered does not exist", "6031071053", 5)
+        addnotify("Script no encontrado (404)", "El script que buscas no existe", "6031071053", 5)
     end
 })
 
 local Section2 = Tab:AddSection({
-    Name = "Loadstring Loader (Use a raw to load the script)"
+    Name = "Loadstring Loader (Usa un link raw para cargar el script)"
 })
 
 Tab:AddTextbox({
-    Name = "Loader: (Only RAWS)",
+    Name = "Loader: (Solo links raw)",
     Default = "",
     TextDisappear = false,
     Callback = function(Value)
@@ -73,7 +74,7 @@ Tab:AddTextbox({
 })
 
 local Section3 = Tab2:AddSection({
-    Name = "Universals scripts"
+    Name = "Scripts Universales"
 })
 
 local function addscript(TabToUse, name, callbacktoscript)
@@ -86,6 +87,36 @@ local function addscript(TabToUse, name, callbacktoscript)
     })
 end
 
-addscript(Tab2, "Infinite yield", function()
+addscript(Tab2, "Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+addscript(Tab2, "Hydroxide", function()
+    local owner = "Upbolt"
+    local branch = "revision"
+
+    local function webImport(file)
+        return loadstring(
+            game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)),
+            file .. '.lua')()
+    end
+
+    webImport("init")
+    webImport("ui/main")
+end)
+
+addscript(Tab2, "Fates Admin", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
+end)
+
+addscript(Tab2, "Orca", function()
+    loadstring(
+        game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua")
+    )()
+end)
+
+addscript(Tab2, "Orca Ultima Snapshot (Esta version puede ser inestable)", function()
+    loadstring(
+        game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/snapshot.lua")
+    )()
 end)
